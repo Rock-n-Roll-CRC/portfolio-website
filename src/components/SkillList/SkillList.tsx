7e12c9950a563c24d16878501e9b25dfd9d9242a
Danil Dikhtyar <danil.dikhtyar@gmail.com>
2026-09-01 13:57:26 +0700

    ✨ Update skill list

--- src/components/SkillList/SkillList.tsx
+++ src/components/SkillList/SkillList.tsx
@@ -2,12 +2,16 @@ import SkillItem from "@/components/SkillItem/SkillItem";
 
 import GearsSVG from "@/assets/icons/gears.svg";
 import CssSVG from "@/assets/icons/logos/css.svg";
+import ExpressSVG from "@/assets/icons/logos/express.svg";
 import FigmaSVG from "@/assets/icons/logos/figma.svg";
 import GitSVG from "@/assets/icons/logos/git.svg";
 import GitHubSVG from "@/assets/icons/logos/github.svg";
 import HtmlSVG from "@/assets/icons/logos/html.svg";
 import JavaScriptSVG from "@/assets/icons/logos/javascript.svg";
+import MongoSVG from "@/assets/icons/logos/mongodb.svg";
 import NextJsSVG from "@/assets/icons/logos/nextjs.svg";
+import NodeSVG from "@/assets/icons/logos/nodejs.svg";
+import PostmanSVG from "@/assets/icons/logos/postman.svg";
 import ReactSVG from "@/assets/icons/logos/react.svg";
 import SassSVG from "@/assets/icons/logos/sass.svg";
 import SupabaseSVG from "@/assets/icons/logos/supabase.svg";
@@ -47,6 +51,22 @@ const skills = [
   {
     icon: SupabaseSVG,
     label: "Supabase",
+  },
+  {
+    icon: NodeSVG,
+    label: "Node.js",
+  },
+  {
+    icon: ExpressSVG,
+    label: "Express.js",
+  },
+  {
+    icon: MongoSVG,
+    label: "MongoDB",
+  },
+  {
+    icon: PostmanSVG,
+    label: "Postman",
   },
   {
     icon: GitSVG,
